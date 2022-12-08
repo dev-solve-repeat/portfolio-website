d46ad9bc826149177e17f15ccec6bea7ac78aede
Brijesh Babu <brijesh.babutv@gmail.com>
2022-12-08 23:27:57 +0530

    Day21: Added content2, description.

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import streamlit as st
 
 st.set_page_config(layout= "wide")
-st.title("Brijesh Babu")
+
 col1, col2 = st.columns(2)
 
 with col1:
     st.image("images/Pic 6.jpg")
 
 with col2:
+    st.title("Brijesh Babu")
     content = """Hi, I am Brijesh Babu. I have done various projects in Python, javascript and React. 
     I am well versedin all these technologies. I have worked with various organisation. 
     One of them is UST Global, in which I had worked in Mainframe Technologies. 
     I love creating new apps using these skills and hope to do more."""
     st.info(content)
+
+content2 = """Below you can find some of the app I have built in Python.
+         Feel free to contact me!"""
+st.write(content2)
