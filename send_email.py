9bd0d00c8147837e353c1b21b5de0839de88efe5
Brijesh Babu <brijesh.babutv@gmail.com>
2022-12-16 12:33:08 +0530

    Day23: Add environmental variable as PASSWORD.

--- send_email.py
+++ send_email.py
@@ -1,11 +1,11 @@
 import smtplib, ssl
-
+import os
 
 def send_mail(message):
     host = "smtp.gmail.com"
     port = 465
     username = "dev.solve.repeat@gmail.com"
-    password = "uaebdeabihqzfvcl"
+    password = os.getenv("PASSWORD")
     receiver = "dev.solve.repeat@gmail.com"
     my_context = ssl.create_default_context()
     with smtplib.SMTP_SSL(host, port, context=my_context) as server:
